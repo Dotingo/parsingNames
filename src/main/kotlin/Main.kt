7b325a49d4eee6f61c3b9d12a39b682aaa8cf4aa
Dotingo <m9132824@gmail.com>
2023-10-25 21:12:31 +0300

    south asia

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,27 +3,24 @@ import org.jsoup.nodes.Document
 import java.io.File
 import java.lang.Exception
 //Spain, German, Greek, Italian
-//Near East: Turkey, Hebrew, Arabic
-//South Asia: India, Nepal
 //Far East: Japanese, China, Korea
 //Central Asia: Kazakhstan, Turkmen, Uzbek, Tajik
-//Oceania
 
 fun parseWebsite() {
-    val url = "https://www.behindthename.com/names/gender/masculine/usage/arabic/relationship/core/"
+    val url = "https://www.behindthename.com/names/gender/masculine/usage/nepali/relationship/core/"
 
     val fileName = "C:\\Users\\m9132\\IdeaProjects\\parsing\\src\\main\\kotlin\\names.txt"
     val file = File(fileName)
 
     val namesList = mutableListOf<String>()
 
- /*   file.forEachLine { line ->
+    file.forEachLine { line ->
         val parts = line.split(", ")
         if (parts.size >= 2) {
             val name = parts[1].trim()
             namesList.add(name)
         }
-    }*/
+    }
     for (page in 1..2) {
         val url = url + page
         try {
@@ -34,7 +31,7 @@ fun parseWebsite() {
                 val rawName = nameElement.text()
                 val cleanedName = rawName.replace(Regex("[0-9]+"), "").trim()
                 if (!namesList.contains(cleanedName)) {
-                    println("Near East, $cleanedName, m")
+                    println("South Asia, $cleanedName, f")
                 }
             }
         } catch (e: Exception) {
